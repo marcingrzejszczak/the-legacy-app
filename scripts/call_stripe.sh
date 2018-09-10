2cc45ddebbd5633ba4655bc5212bee1e63fb80e1
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-09-10 16:23:13 +0200

    Polish

--- scripts/call_stripe.sh
+++ scripts/call_stripe.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl https://api.stripe.com/v1/charges?limit=25x` -u sk_test_4eC39HqLyjWDarjtT1zdp7dc: | jq
+curl https://api.stripe.com/v1/charges?limit=25 -u sk_test_4eC39HqLyjWDarjtT1zdp7dc: | jq
